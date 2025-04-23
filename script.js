422107f0dbdea28bc54160fcd5c1c35653b37e2d
arielmusa <arielmusainfo@gmail.com>
2025-04-23 16:14:41 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const apiUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+function generateRandomEMails(number) {
+  const randomEmailsGenerated = [];
+  for (let i = 0; i < number; i++) {
+    axios.get(apiUrl).then(function (response) {
+      randomEmailsGenerated.push(response.data.response);
+    });
+  }
+  return randomEmailsGenerated;
+}
+
+let randomEmails = [];
+
+randomEmails.push(generateRandomEMails(10));
+
+console.log(randomEmails);
