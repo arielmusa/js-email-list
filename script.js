8d4504183abdb1160750ede1cba76a66045dee86
arielmusa <arielmusainfo@gmail.com>
2025-04-23 17:16:22 +0200

    script.js: display generated email list in list-group by clicking a button. A second click on the button, removes the previous elements, the follow up click will generate a new list (to fix)

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// get DOM elements
+const listGroup = document.getElementById("list-group");
+
+const generateEmailBtn = document.getElementById("generate-email-list");
+
+// API URL
 const apiUrl = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 function generateRandomEMails(number) {
@@ -9,7 +15,35 @@ function generateRandomEMails(number) {
   }
   return randomEmailsGenerated;
 }
-
 let randomEmails = generateRandomEMails(10);
-
 console.log(randomEmails);
+
+/* async function displayRandomEmails() {
+  for (let i = 0; i < randomEmails.length; i++) {
+    currentEmail = randomEmails[i];
+    const li = document.createElement("li");
+    li.textContent = currentEmail;
+    listGroup.appendChild(li);
+    console.log(currentEmail);
+  }
+} */
+
+function generateListItems() {
+  for (let i = 0; i < randomEmails.length; i++) {
+    currentEmail = randomEmails[i];
+    const li = document.createElement("li");
+    li.textContent = currentEmail;
+    li.classList.add("list-group-item");
+    listGroup.appendChild(li);
+  }
+}
+
+generateEmailBtn.addEventListener("click", () => {
+  const listItems = listGroup.querySelectorAll("li");
+  if (listItems.length === 0) return generateListItems();
+  if (listItems.length > 0) {
+    listItems.forEach((item) => item.remove());
+    randomEmails = generateRandomEMails(10);
+    generateListItems();
+  }
+});
